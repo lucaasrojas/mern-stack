8b4c11559b6c857de557a82d18aeccf30d38bb7f
unknown <lucas.rojas@live.com.ar>
2020-08-17 16:53:55 -0300

    Fix building

--- src/app/index.js
+++ src/app/index.js
@@ -5,7 +5,7 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import MainPage from './components/MainPage'
 
-//import './styles/general.css'
+import './styles/general.scss'
 
 class App extends Component {
     render() {
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     entry: './src/app/index.js',
     output: {
-        path: __dirname + '/public/',
+        path: path.resolve(__dirname,'public/'),
         filename: 'bundle.js'
     },
     module: {
@@ -12,7 +13,20 @@ module.exports = {
                 loader: 'babel-loader',
                 test: /\.js$/,
                 exclude: /node_modules/
+            },
+            {
+                test: /\.(css|sass|scss)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
             }
         ]
-    }
+    },
+    plugins:[
+        new MiniCssExtractPlugin({
+            filename: 'styles.css'
+        })
+    ]
 }
